ee3ad84eb60e9980828fc2c808c2885250905f27
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-07-16 10:59:22 +0530

    activity_02
    done

---
+++ Day_05_activity/activity_02.py
@@ -0,0 +1,4 @@
+# print a rocket using single print()statement
+# o/p : 5-->4-->3-->2-->1-->Liftoff!
+
+print("5","4","3","2","1","Liftoff!",sep="-->")
--- Day_05_activity/demo.py
+++ Day_05_activity/demo.py
@@ -4,3 +4,16 @@ print("greater is:",2>3)
 print("greater is:",int(2>3))
 #newline(\n)
 print("Hello, \nWorld")
+
+#custom separators:(sep) 
+print("2025","07","16",sep="-")
+
+# custom ending:(end)
+print("sathya",end="...")
+print("rangaraj")
+
+# name="sathya"
+# marks=90
+# print
+
+
