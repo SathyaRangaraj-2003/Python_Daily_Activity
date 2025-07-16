694ac935b2c2a0cf2b63ad849aab0eec7c03fc07
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-07-16 18:42:46 +0530

    done

--- Day_05_activity/activity_08.py
+++ Day_05_activity/activity_08.py
@@ -1,6 +1,6 @@
 playlist=["TrackA","TrackB","TrackC","TrackD"]
 playlist.remove('TrackC')
 playlist.insert(0,'TrackX')
-poped_str=playlist.pop(-1)
-playlist.insert(1,poped_str)
+popped_str=playlist.pop(-1)
+playlist.insert(1,popped_str)
 print(*playlist)
