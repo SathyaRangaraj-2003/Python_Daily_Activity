31094a771266226939081f941c8856dfc2c29722
SathyaRangaraj-2003 <sathya.2.rangaraj@coforge.com>
2025-07-17 12:28:29 +0530

    activity 2

--- Day_06_activity/demo.py
+++ Day_06_activity/demo.py
@@ -4,7 +4,7 @@
 # sathya_tuple=("apple",2,None,True,5.9,[1,2],5+8,1>>2,3>8)
 # print(sathya_tuple)
 
-#list
+# # list
 # list=[1,2,3,4]
 # tuple=(list[0:3])
 # print(tuple)
@@ -12,7 +12,19 @@
 # t1=(1,2,3,4,5)
 # print(t1)
 
-#by tuple
-list=tuple([1,2,3,4])
-tuple=(list[0:3])
-print(tuple)
+# #by tuple
+# list=tuple([1,2,3,4])
+# tuple=(list[0:3])
+# print(tuple)
+
+# #tuple with list
+# x=tuple([1,2])
+# print(x)
+
+# #nested tuple
+# x=((1,2),(3,4))
+# print(x[0])
+
+#nested tuple by negative indexing
+x=((1,2),(3,4))
+print(x[-1][-2])
